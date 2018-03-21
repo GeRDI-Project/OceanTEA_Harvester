77295e5fa2d0b22015e09900a9af91309a52f06c
Ingo Thomsen <int@informatik.uni-kiel.de>
2018-03-21 15:58:48 +0100

    Added workaround for missing unit for 'Potential Density Anomaly'

--- src/main/java/de/gerdiproject/harvest/oceantea/utils/OceanTeaDownloader.java
+++ src/main/java/de/gerdiproject/harvest/oceantea/utils/OceanTeaDownloader.java
@@ -56,6 +56,9 @@ public final class OceanTeaDownloader
         AllDataTypesResponse allDatatypesResponse = HTTP_REQUESTER
                                                     .getObjectFromUrl(OceanTeaTimeSeriesDownloaderConstants.DATATYPES_URL, AllDataTypesResponse.class);
 
+        // Workaround: Setting one missing unit, which is not given by the OceanTEA (demo) site
+        allDatatypesResponse.getPotentialDensityAnomaly().setUnit("kg/m^3");
+
         return JsonResponsesMerger.getAllTimeSeries(allTimeSeriesResponse, allDatatypesResponse);
     }
 
