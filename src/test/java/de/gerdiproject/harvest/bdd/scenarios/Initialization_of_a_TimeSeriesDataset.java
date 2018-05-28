629e14a70297643890567babfd522a866bbda0d5
Ingo Thomsen <int@informatik.uni-kiel.de>
2018-05-28 11:56:26 +0200

    SAI-312 BDD scenario for a unit test: Initalization of a TimeSeriesDataset

---
+++ src/test/java/de/gerdiproject/harvest/bdd/scenarios/Initialization_of_a_TimeSeriesDataset.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright © 2018 Ingo Thomsen (http://www.gerdi-project.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.gerdiproject.harvest.bdd.scenarios;
+
+import java.time.Instant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.jgiven.junit.ScenarioTest;
+
+import de.gerdiproject.harvest.bdd.stages.oceantea.json.GivenTimeSeriesDatasetResponse;
+import de.gerdiproject.harvest.bdd.stages.oceantea.utils.ThenTimeSeriesDataset;
+import de.gerdiproject.harvest.bdd.stages.oceantea.utils.WhenTimeSeriesDataset;
+import de.gerdiproject.harvest.bdd.tags.Tag;
+
+@Tag("Initialization")
+@RunWith(DataProviderRunner.class)
+public class Initialization_of_a_TimeSeriesDataset extends ScenarioTest<GivenTimeSeriesDatasetResponse, WhenTimeSeriesDataset, ThenTimeSeriesDataset>
+{
+
+    @Test
+    public void minimal_number_of_entries()
+    {
+        given().a_TimeSeriesDatasetResponse_with_boundary_time_offsets_$_and_$(-100, 100);
+        when().a_TimeSeriesDataset_is_created_for_Instant(Instant.parse("2013-05-30T23:38:23Z"));
+        then().the_TimeSeriesDataset_has_at_least_$_values(2);
+    }
+
+    @Test
+    public void missing_time_series_data()
+    {
+        given().an_empty_TimeSeriesDatasetResponse();
+        when().a_TimeSeriesDataset_is_created_for_Instant(Instant.parse("2013-05-30T23:38:23Z"));
+        then().the_TimeSeriesDataset_has_no_values().and().startInstant_and_stopInstant_are_equal();
+    }
+
+    @Test
+    public void only_one_time_series_entry()
+    {
+        given().an_TimeSeriesDatasetResponse_with_one_random_value_pair();
+        when().a_TimeSeriesDataset_is_created_for_Instant(Instant.parse("2013-05-30T23:38:23Z"));
+        then().startInstant_and_stopInstant_are_equal();
+    }
+
+    /**
+     * The {@linkplain Instant} are given as strings in ISO-8601 format
+     */
+    @DataProvider({
+        // @formatter:off
+        "2013-05-30T23:38:23Z,  0,  0, 2013-05-30T23:38:23Z, 2013-05-30T23:38:23Z",
+        "2013-05-30T23:59:30Z, 30,-30, 2013-05-30T23:59:00Z, 2013-05-31T00:00:00Z",
+        "2013-05-30T23:38:23Z, 30,-30, 2013-05-30T23:37:53Z, 2013-05-30T23:38:53Z",
+        // "2014-05-30T23:38:23Z, 30,-30, 2013-05-30T23:37:53Z, 2013-05-30T23:38:53Z", // this would be a failing case for example
+        // @formatter:on
+    })
+    @Test
+    public void calculation_of_start_and_stop_instant(
+        String referenceInstant,
+        int offset1,
+        int offset2,
+        String startInstant,
+        String stopInstant
+    )
+    {
+        given().a_TimeSeriesDatasetResponse_with_boundary_time_offsets_$_and_$(offset1, offset2);
+
+        when().a_TimeSeriesDataset_is_created_for_Instant(Instant.parse(referenceInstant));
+
+        then().the_TimeSeriesDataset_startInstant_is_$(Instant.parse(startInstant)).and()
+        .the_TimeSeriesDataset_stopInstant_is_$(Instant.parse(stopInstant));
+    }
+}
