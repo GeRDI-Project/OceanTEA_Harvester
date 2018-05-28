6f6a6d44d94d9d21251856165f38b4caceeb38c7
Ingo Thomsen <int@informatik.uni-kiel.de>
2018-05-28 11:58:48 +0200

    SAI-312 BDD stages for unit tests (organized in tree matching the resp. classes)

--- src/test/java/de/gerdiproject/harvest/bdd/stages/oceantea/utils/WhenTimeSeriesDataset.java
+++ src/test/java/de/gerdiproject/harvest/bdd/stages/oceantea/utils/WhenTimeSeriesDataset.java
@@ -1,4 +1,19 @@
-package de.gerdiproject.harvest.bdd.stages;
+/**
+ * Copyright © 2018 Ingo Thomsen (http://www.gerdi-project.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.gerdiproject.harvest.bdd.stages.oceantea.utils;
 
 import java.time.Instant;
 
@@ -18,43 +33,9 @@ public class WhenTimeSeriesDataset extends Stage<WhenTimeSeriesDataset>
     @ProvidedScenarioState
     TimeSeriesDataset timeSeriesDataset;
 
-
     public WhenTimeSeriesDataset a_TimeSeriesDataset_is_created_for_Instant(Instant referenceInstant)
     {
         timeSeriesDataset = new TimeSeriesDataset(timeSeriesDatasetResponse, referenceInstant);
         return self();
-
     }
-
-    // @ProvidedScenarioState
-    // int returnValue;
-    //
-    // @ProvidedScenarioState
-    // ArithmeticException caughtArithmeticException;
-    //
-    // public void I_add_4_and_2()
-    // {
-    //
-    // // returnValue = simpleMathObject.add(4, 2);
-    //
-    // }
-    //
-    // public void I_add_$_and_$(int a, int b)
-    // {
-    //
-    // // returnValue = simpleMathObject.add(a, b);
-    //
-    // }
-    //
-    // public void I_divide_1_by_0()
-    // {
-    //
-    // try {
-    // // returnValue = simpleMathObject.divide(1, 0);
-    // } catch (ArithmeticException e) {
-    //
-    // caughtArithmeticException = e;
-    // }
-    // }
-
 }
