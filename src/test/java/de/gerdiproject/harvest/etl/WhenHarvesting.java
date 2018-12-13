ff312e627e288560f0e2545b30d8d42f2bbc4d5d
Ingo Thomsen <int@informatik.uni-kiel.de>
2018-12-13 10:46:41 +0100

    SAI-1204 Harvesting When stage now works with new TimeSeriesExtractor

--- src/test/java/de/gerdiproject/harvest/etl/WhenHarvesting.java
+++ src/test/java/de/gerdiproject/harvest/etl/WhenHarvesting.java
@@ -1,19 +1,14 @@
 /**
- * Copyright © 2018 Ingo Thomsen (http://www.gerdi-project.de)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright © 2018 Ingo Thomsen (http://www.gerdi-project.de) Licensed under
+ * the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable
+ * law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
-//package de.gerdiproject.harvest.bdd.stages_integration;
+// package de.gerdiproject.harvest.bdd.stages_integration;
 package de.gerdiproject.harvest.etl;
 
 import java.util.ArrayList;
@@ -21,7 +16,7 @@ import java.util.List;
 
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
-import org.powermock.reflect.Whitebox;
+import org.powermock.api.mockito.PowerMockito;
 
 import com.google.gson.Gson;
 import com.tngtech.jgiven.Stage;
@@ -36,7 +31,6 @@ import de.gerdiproject.harvest.etls.extractors.TimeSeriesExtractor;
 import de.gerdiproject.harvest.etls.loaders.AbstractIteratorLoader;
 import de.gerdiproject.harvest.etls.loaders.LoaderException;
 import de.gerdiproject.harvest.etls.loaders.events.CreateLoaderEvent;
-import de.gerdiproject.harvest.etls.transformers.TimeSeriesTransformer;
 import de.gerdiproject.harvest.event.EventSystem;
 import de.gerdiproject.harvest.oceantea.json.AllDataTypesResponse;
 import de.gerdiproject.harvest.oceantea.json.AllTimeSeriesResponse;
@@ -45,11 +39,12 @@ import de.gerdiproject.harvest.utils.data.HttpRequester;
 import de.gerdiproject.json.datacite.DataCiteJson;
 
 /**
- * This When stage starts the actual harvesting of the given JSON response
- * strings. For this the retrieval via HTTP request from OceanTEA is intercepted
- * using mocking.
+ * This When stage provides the step method to start the actual harvesting of
+ * the given JSON response strings. For this a mock {@linkplain HttpRequester}
+ * is created for the {@linkplain TimeSeriesExtractor} that returns the test
+ * JSON data instead of calling OceanTEA.
  *
- * @author Ingo Thomsen
+ * @author Ingo Thomsen, Robin Weiss
  */
 public class WhenHarvesting extends Stage<WhenHarvesting>
 {
@@ -69,8 +64,32 @@ public class WhenHarvesting extends Stage<WhenHarvesting>
 
 
     /**
-     * Private generic helper to define for a mocked HttpRequester the behavior of
-     * the getObjectFromUrl() method: If requesting a JSON response for a specific
+     * Create a mock HttpRequester (once before calling any steps): Set up stubbed
+     * methods to return the JSON strings provided as scenario state variables.
+     */
+    @BeforeStage
+    public void prepareHarvesterLibaryForMockAccess()
+    {
+        // create a mock HttpRequester for intercepting JSON requests
+        HttpRequester mockHttpRequester = PowerMockito.mock(HttpRequester.class);
+
+        // set returned objects (created from JSON strings) for the stubbed methods
+        setStubReturnValue(mockHttpRequester, AllTimeSeriesResponse.class, allTimeSeriesJSONResponse);
+        setStubReturnValue(mockHttpRequester, AllDataTypesResponse.class, allDataTypesJSONResponse);
+        setStubReturnValue(mockHttpRequester, TimeSeriesDatasetResponse.class, timeSeriesDatasetJSONResponse);
+
+        try {
+            // intercept the constructor call for HttpRequester to return the mock instead.
+            PowerMockito.whenNew(HttpRequester.class).withAnyArguments().thenReturn(mockHttpRequester);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not mock call to constructor of class HttpRequester", e);
+        }
+    }
+
+
+    /**
+     * Private generic helper to define for a mock HttpRequester the behavior of the
+     * getObjectFromUrl() method: If requesting a JSON response for a specific
      * targetClass the respective JSON string - provided through the scenario state
      * variable - is returned.
      *
@@ -86,29 +105,10 @@ public class WhenHarvesting extends Stage<WhenHarvesting>
 
 
     /**
-     * Create a mock HttpRequester, set up stubbed methods to return the JSON
-     * strings (provided as ScenarioState).
+     * Step method for harvesting (extraction & transformation) of test JSON string
      */
-    @BeforeStage
-    public void prepareHarvesterLibaryForMockAccess()
-    {
-        // create a mock HttpRequester for intercepting JSON requests
-        HttpRequester mockHttpRequester = Mockito.mock(HttpRequester.class);
-
-        // set returned objects (created from JSON strings) for the stubbed methods
-        setStubReturnValue(mockHttpRequester, AllTimeSeriesResponse.class, allTimeSeriesJSONResponse);
-        setStubReturnValue(mockHttpRequester, AllDataTypesResponse.class, allDataTypesJSONResponse);
-        setStubReturnValue(mockHttpRequester, TimeSeriesDatasetResponse.class, timeSeriesDatasetJSONResponse);
-
-        // inject mock into the static OceanTeaDownloader
-        Whitebox.setInternalState(TimeSeriesExtractor.class, HttpRequester.class, mockHttpRequester);
-        Whitebox.setInternalState(TimeSeriesTransformer.class, HttpRequester.class, mockHttpRequester);
-    }
-
-
     public WhenHarvesting harvested()
     {
-
         // make sure harvested documents are loaded into 'resultingIDocuments'
         EventSystem.addSynchronousListener(CreateLoaderEvent.class, (CreateLoaderEvent e) -> new MockedLoader());
 
@@ -125,9 +125,9 @@ public class WhenHarvesting extends Stage<WhenHarvesting>
         return self();
     }
 
-
     /**
-     * This test Loader stores harvested documents in 'resultingIDocuments'.
+     * This mockedLoader stores harvested documents in scenario state variable
+     * 'resultingIDocuments'.
      *
      * @author Robin Weiss
      */
